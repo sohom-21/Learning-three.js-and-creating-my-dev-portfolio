329e8939c7ff9199fb86dd3fbf75101b8188d744
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-08-06 05:00:31 +0530

    Replace app div with canvas element and implement Three.js scene setup with camera, lighting, and animation loop

--- hero_threeproject/src/main.js
+++ hero_threeproject/src/main.js
@@ -1,22 +1,56 @@
 import './style.css'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+// 1. Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#f0f0f0');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// 2. Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
-setupCounter(document.querySelector('#counter'))
+// 3. Object
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+// 4. Lighting
+const light = new THREE.SpotLight(0x006769, 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Renderer
+const  renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio); // Useful for high DPI displays
+
+// 6. Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// Adding objects to the scene
+scene.add(cube);
+
+// 7. Animation Loop
+const animate = () => {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.001;
+    controls.update();
+    renderer.render(scene, camera);
+};
+// Handle window resize -- the animation should adapt to the new size of the window if it changes
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
