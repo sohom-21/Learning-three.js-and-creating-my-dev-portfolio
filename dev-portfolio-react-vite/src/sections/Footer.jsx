aa634b289c6d84d99b75315a2a5fc0f3a954f16e
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-11-11 19:55:15 +0530

    feat: refactor social media links in Footer component for improved accessibility

--- dev-portfolio-react-vite/src/sections/Footer.jsx
+++ dev-portfolio-react-vite/src/sections/Footer.jsx
@@ -8,18 +8,18 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
-        <div className="social-icon">
+        <a href="https://github.com/sohom-21" target="_blank" rel="noopener noreferrer" className="social-icon">
           <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
+        </a>
+        <a href="https://x.com/SohomG17460?t=lbcqGvLjJgZVh51SMzhibg&s=09" target="_blank" rel="noopener noreferrer" className="social-icon">
           <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
+        </a>
+        <a href="https://www.facebook.com/share/1CQf5AEhMu/" target="_blank" rel="noopener noreferrer" className="social-icon">
           <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
+        </a>
+        <a href="https://www.linkedin.com/in/sohom-ghosh-3894b130a" target="_blank" rel="noopener noreferrer" className="social-icon">
           <img src="/assets/linkedin_icon.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        </a>
       </div>
 
       <p className="text-white-500">© 2025 Sohom Ghosh. All rights reserved.</p>
